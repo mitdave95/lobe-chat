c68cc8a9ddb5d0766de09e00072224b0277048b3
Mit Dave <mitdave95@gmail.com>
2024-08-02 19:15:49 -0700

    Sentient Model list updated

--- src/config/modelProviders/deepseek.ts
+++ src/config/modelProviders/deepseek.ts
@@ -4,26 +4,38 @@ import { ModelProviderCard } from '@/types/llm';
 const DeepSeek: ModelProviderCard = {
   chatModels: [
     {
-      description: '擅长通用对话任务',
-      displayName: 'DeepSeek-V2',
+      description: 'Llama 3.1 Instruct 8B',
+      displayName: 'Llama 3.1 Instruct 8B',
       enabled: true,
-      functionCall: true,
-      id: 'deepseek-chat',
-      tokens: 128_000,
+      id: 'meta.llama3-1-8b-instruct-v1:0',
+      tokens: 4096,
     },
     {
-      description: '擅长处理编程和数学任务',
-      displayName: 'DeepSeek-Coder-V2',
+      description: 'Llama 3.1 Instruct 70B',
+      displayName: 'Llama 3.1 Instruct 70B',
       enabled: true,
-      functionCall: true,
-      id: 'deepseek-coder',
-      tokens: 128_000,
+      id: 'meta.llama3-1-70b-instruct-v1:0',
+      tokens: 4096,
+    },
+    {
+      description: 'Llama 3.1 Instruct 405B',
+      displayName: 'Llama 3.1 Instruct 405B',
+      enabled: true,
+      id: 'meta.llama3-1-405b-instruct-v1:0',
+      tokens: 4096,
+    },
+    {
+      description: 'Mistral Large 2',
+      displayName: 'Mistral Large 2',
+      enabled: true,
+      id: 'mistral.mistral-large-2407-v1:0',
+      tokens: 4096,
     },
   ],
-  checkModel: 'deepseek-chat',
+  checkModel: 'meta.llama3-1-8b-instruct-v1:0',
   id: 'deepseek',
-  modelList: { showModelFetcher: true },
-  name: 'DeepSeek',
+  disableBrowserRequest: true,
+  name: 'Sentient',
 };
 
 export default DeepSeek;
