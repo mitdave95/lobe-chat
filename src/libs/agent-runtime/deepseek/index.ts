0dccb3ac073078c5a1a82ba326d0d69cba9d211f
Mit Dave <mitdave95@gmail.com>
2024-08-02 19:14:25 -0700

    Updated Sentient API URL

--- src/libs/agent-runtime/deepseek/index.ts
+++ src/libs/agent-runtime/deepseek/index.ts
@@ -2,7 +2,7 @@ import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
 export const LobeDeepSeekAI = LobeOpenAICompatibleFactory({
-  baseURL: 'https://api.deepseek.com/v1',
+  baseURL: 'https://hp3hebj84f.us-west-2.awsapprunner.com/v1',
   debug: {
     chatCompletion: () => process.env.DEBUG_DEEPSEEK_CHAT_COMPLETION === '1',
   },
